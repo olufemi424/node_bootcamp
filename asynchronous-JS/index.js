c16a011e8e891d17e8da09fe23d36ed92ebbb1e9
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2019-09-17 21:44:12 -0500

    async await

--- asynchronous-JS/index.js
+++ asynchronous-JS/index.js
@@ -1,19 +1,54 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) {
-        return console.log(err.message);
-      }
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, err => {
-        console.log('Random dog image saved');
-      });
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 🔥');
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('dog-img.txt', data, err => {
+      if (err) reject('I could not write that file 🔥');
+      resolve('Random Dog Image Successfully saved');
+    });
+  });
+};
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then(data => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then(res => {
+//     console.log(res.body);
+//     return writeFilePro(`${__dirname}/dog.txt`, res.body.message);
+//   })
+//   .then(result => console.log(result))
+//   .catch(err => {
+//     console.log(err.message);
+//   });
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+
+    console.log(`Breed: ${data}`);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const final = await writeFilePro(`${__dirname}/dog.txt`, res.body.message);
+
+    console.log(final);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getDogPic();
